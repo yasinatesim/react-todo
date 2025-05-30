6c127be8357e76f6bbf806c6bdb7ce79ca2722a9
Yasin ATEŞ <yasinatesim@gmail.com>
2025-05-30 14:21:03 +0300

    feat: add build folder for vercel to netlify migration

---
+++ build/precache-manifest.d720e2b1e646f2fb5ad5afa4b8f9e9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ab625bbf817f460da5fd733b61cec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e45d8ad3f53e04b93380",
+    "url": "/static/css/main.85b290ad.chunk.css"
+  },
+  {
+    "revision": "a534fecdad77e9d3b575",
+    "url": "/static/js/2.e85d0cb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e85d0cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e45d8ad3f53e04b93380",
+    "url": "/static/js/main.d2db3fc9.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  }
+]);
